cf9abb95ad645f1cd8cc76146f338f97711136d6
dan.markhasin <dan.markhasin@appsflyer.com>
2021-10-15 12:10:40 +0300

    print type and value

--- constraint/constraint.go
+++ constraint/constraint.go
@@ -6,42 +6,14 @@ type Addable interface {
 	type int, float64, string
 }
 
-type Helloer interface {
-	Hello() string
-}
-
-type Person struct {
-	Name string
-}
-
-func (p Person) Hello() string {
-	return "hello"
-}
-
-
 func sum[T Addable](a, b T) T {
 	return a + b
 }
 
-
-func helloGeneric[T Helloer](a T) T {
-	return a
-}
-
-func hello(a Helloer) Helloer {
-	return a
-}
-
 func main() {
-	fmt.Println(sum(10, 30)) // arguments must be of the same type
-	fmt.Println(sum(1.5, 4.2))
-	fmt.Println(sum("foo", "bar"))
-
-
-	fmt.Println(helloGeneric(&Person{Name: "Dan"}).Name)
-
-	// uncomment for erro
-	fmt.Printf("%T", hello(&Person{Name: "Dan"}).Name)
+	fmt.Printf("%[1]T %[1]v\n", sum(10, 30)) // arguments must be of the same type
+	fmt.Printf("%[1]T %[1]v\n", sum(1.5, 4.2))
+	fmt.Printf("%[1]T %[1]v\n", sum("foo", "bar"))
 
 	// uncomment for error
 	// fmt.Println(sum("foo", 1))
